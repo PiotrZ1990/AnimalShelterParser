4f698866ed49c4b726cedb2d8ff647f106f23ed4
Piotr Zboś <piotr.zbos@o2.pl>
2024-08-08 22:13:16 +0200

    wszystko na 1 arkuszu

--- lib/animal_parser.rb
+++ lib/animal_parser.rb
@@ -3,8 +3,8 @@ require 'httparty'
 require 'write_xlsx'
 
 class AnimalParser
-  def initialize(url)
-    @url = url
+  def initialize(base_url)
+    @base_url = base_url
   end
 
   def fetch_data(url)
@@ -16,59 +16,8 @@ class AnimalParser
     page_number = 1
     animals = []
 
-      loop do
-      url = "https://schroniskochorzow.pl/psy/page/#{page_number}/"
-      doc = fetch_data(url)
-      items = doc.css('.rt-grid-item')
-
-      break if items.empty?
-
-      doc.css('.rt-grid-item').each do |animal|
-        name = animal.css('h2 strong').text.strip
-        details = animal.css('p').text.strip
-        image_url = animal.css('.rt-img-holder img').attr('src')&.value
-
-        # Debugging output
-        puts "Dog details: #{details.inspect}"
-        puts "Image URL: #{image_url.inspect}"
-
-        # Extracting information using regex
-        number = details.match(/Numer:\s*([\d\/-]+)/)&.captures&.first&.strip
-        gender = details.match(/Płeć:\s*(samiec|samica|samce|samice)/)&.captures&.first&.strip
-
-        # Extract birth year and month (if available)
-        birth_year_match = details.match(/Wiek:\s*ur\.\s*(?:\d{2}\.)?\s*(\d{4})/)
-        if birth_year_match
-          year = birth_year_match[1]   # Extract year
-          birth_year = "ur. #{year}"   # Format as "ur. YYYY"
-        else
-          birth_year = nil
-        end
-
-        # Size (Rozmiar) extraction, if applicable
-        size = details.match(/Rozmiar:\s*(.*)/)&.captures&.first&.strip
-
-        animals << {
-          name: name,
-          number: number,
-          gender: gender,
-          birth_year: birth_year,
-          size: size,
-          image_url: image_url
-        }
-      end
-
-      page_number += 1
-    end
-      animals
-    end
-
-  def parse_cats
-    page_number = 1
-    animals = []
-
     loop do
-      url = "https://schroniskochorzow.pl/koty/page/#{page_number}/"
+      url = "#{@base_url}/psy/page/#{page_number}/"
       doc = fetch_data(url)
       items = doc.css('.rt-grid-item')
 
@@ -79,16 +28,50 @@ class AnimalParser
         details = animal.css('p').text.strip
         image_url = animal.css('.rt-img-holder img').attr('src')&.value
 
-        # Debugging output
-        puts "Cat details: #{details.inspect}"
-        puts "Image URL: #{image_url.inspect}"
+        # Extracting information using regex
+        number = details.match(/Numer:\s*([\d\/-]+)/)&.captures&.first&.strip
+        gender = details.match(/Płeć:\s*(samiec|samica|samce|samice)/)&.captures&.first&.strip
+        birth_year_match = details.match(/Wiek:\s*ur\.\s*(?:\d{2}\.)?\s*(\d{4})/)
+        birth_year = birth_year_match ? "ur. #{birth_year_match[1]}" : nil
+        size = details.match(/Rozmiar:\s*(.*)/)&.captures&.first&.strip
+
+        animals << {
+          name: name,
+          number: number,
+          gender: gender,
+          age: birth_year,
+          size: size,
+          image_url: image_url,
+          species: 'Pies'
+        }
+      end
+
+      page_number += 1
+    end
+
+    animals
+  end
+
+  def parse_cats
+    page_number = 1
+    animals = []
+
+    loop do
+      url = "#{@base_url}/koty/page/#{page_number}/"
+      doc = fetch_data(url)
+      items = doc.css('.rt-grid-item')
+
+      break if items.empty?
+
+      items.each do |animal|
+        name = animal.css('h2 strong').text.strip
+        details = animal.css('p').text.strip
+        image_url = animal.css('.rt-img-holder img').attr('src')&.value
 
         # Extracting information using regex
         number = details.match(/Numer:\s*([\d\/-]+)/)&.captures&.first&.strip
         gender = details.match(/Płeć:\s*(samiec|samica)/)&.captures&.first&.strip
         age = details.match(/Wiek:\s*(.*?)\s*(Znaleziona|Znaleziony|$)/)&.captures&.first&.strip
-
-        # Extract FIV/FELV test result with various possible formats
         fiv_felv_test_match = details.match(/Test FIV\/FELV\s*\([^\)]+\)|Test FIV\s*\([^\)]+\)\s*\/\s*FELV\s*\([^\)]+\)/)
         fiv_felv_test = fiv_felv_test_match ? fiv_felv_test_match[0].strip : 'Brak testu'
 
@@ -98,7 +81,8 @@ class AnimalParser
           gender: gender,
           age: age,
           fiv_felv_test: fiv_felv_test,
-          image_url: image_url
+          image_url: image_url,
+          species: 'Kot'
         }
       end
 
@@ -109,7 +93,8 @@ class AnimalParser
   end
 
   def parse_new_arrivals
-    doc = fetch_data(@url)
+    url = "#{@base_url}/nowo-przyjete/"
+    doc = fetch_data(url)
     animals = []
 
     doc.css('.rt-grid-item').each do |animal|
@@ -117,54 +102,52 @@ class AnimalParser
       details = animal.css('p').text.strip
       image_url = animal.css('.rt-img-holder img').attr('src')&.value
 
-      # Debugging output
-      puts "New arrival details: #{details.inspect}"
-      puts "Number: #{number.inspect}"
-      puts "Image URL: #{image_url.inspect}"
-
-      # Wyciąganie informacji przy użyciu regex
+      # Extracting information using regex
       quarantine_until = details.match(/Kwarantanna do:\s*(\d{2}\.\d{2}\.\d{4})/)&.captures&.first&.strip || 'Brak daty'
       gender = details.match(/Płeć:\s*(samiec|samica)/)&.captures&.first&.strip || 'Brak płci'
       age = details.match(/Wiek:\s*(.*?)\s*(Znaleziona|Znaleziony|$)/)&.captures&.first&.strip || 'Brak wieku'
       found = details.match(/(Znaleziona|Znaleziony):\s*(.*)/)&.captures&.last&.strip || 'Brak miejsca'
 
       animals << {
+        name: number,
         number: number,
-        quarantine_until: quarantine_until,
         gender: gender,
         age: age,
         found: found,
-        image_url: image_url
+        image_url: image_url,
+        species: 'Nowo przybyłe'
       }
     end
 
     animals
   end
 
-  def generate_excel(dogs_data, cats_data, new_arrivals_data)
+  def generate_excel(all_data)
     workbook = WriteXLSX.new('Schronisko Chorzow.xlsx')
+    sheet = workbook.add_worksheet('Zwierzęta')
 
-    # Dogs sheet
-    dogs_sheet = workbook.add_worksheet('Psy')
-    dogs_sheet.write_row(0, 0, ["Imię", "Numer", "Płeć", "Wiek", "Rozmiar", "URL zdjęcia"])
-    dogs_data.each_with_index do |animal, index|
-      dogs_sheet.write_row(index + 1, 0, [animal[:name], animal[:number], animal[:gender], animal[:birth_year], animal[:size], animal[:image_url]])
-    end
+    sheet.write_row(0, 0, ["Imię", "Numer", "Płeć", "Wiek", "Rozmiar", "Testy", "Znaleziona", "Gatunek", "URL zdjęcia"])
 
-    # Cats sheet
-    cats_sheet = workbook.add_worksheet('Koty')
-    cats_sheet.write_row(0, 0, ["Imię", "Numer", "Płeć", "Wiek", "Testy", "URL zdjęcia"])
-    cats_data.each_with_index do |animal, index|
-      cats_sheet.write_row(index + 1, 0, [animal[:name], animal[:number], animal[:gender], animal[:age], animal[:fiv_felv_test], animal[:image_url]])
-    end
-
-    # New arrivals sheet
-    new_arrivals_sheet = workbook.add_worksheet('Nowo przyjęte')
-    new_arrivals_sheet.write_row(0, 0, ["Numer", "Kwarantanna do:", "Płeć", "Wiek", "Znaleziona", "URL zdjęcia"])
-    new_arrivals_data.each_with_index do |animal, index|
-      new_arrivals_sheet.write_row(index + 1, 0, [animal[:number], animal[:quarantine_until], animal[:gender], animal[:age], animal[:found], animal[:image_url]])
+    all_data.each_with_index do |animal, index|
+      sheet.write_row(index + 1, 0, [
+        animal[:name], animal[:number], animal[:gender], animal[:age], animal[:size], 
+        animal[:fiv_felv_test], animal[:found], animal[:species], animal[:image_url]
+      ])
     end
 
     workbook.close
   end
+
+  def parse_all
+    dogs = parse_dogs
+    cats = parse_cats
+    new_arrivals = parse_new_arrivals
+
+    generate_excel(dogs + cats + new_arrivals)
+  end
 end
+
+# Użycie parsera
+base_url = 'https://schroniskochorzow.pl'
+parser = AnimalParser.new(base_url)
+parser.parse_all
